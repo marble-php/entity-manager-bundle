3b8861cd6240ab613c263061131265125ac7dab1
Mark van den Berg <mark.van.den.berg@autarco.com>
2026-02-08 14:04:07 +0100

    declare strict types

--- src/DefaultEntityIoProvider.php
+++ src/DefaultEntityIoProvider.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Marble\EntityManager\Bundle;
 
 use Marble\Entity\Entity;
--- src/DependencyInjection/DetectCustomRepositoriesPass.php
+++ src/DependencyInjection/DetectCustomRepositoriesPass.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Marble\EntityManager\Bundle\DependencyInjection;
 
 use Marble\Entity\Entity;
